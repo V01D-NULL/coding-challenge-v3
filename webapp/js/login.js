cd444b061c08d2d137c7d99b3699a1177ea36504
Tim Thompson <timthompson@CoS092TThompson.localdomain>
2022-10-17 14:39:13 -0500

    Convert login page to react

--- webapp/js/login.js
+++ webapp/js/login.js
@@ -1,11 +1,74 @@
 "use strict";
 
-document.getElementById('sign-up').addEventListener("submit", (event) => {
-    event.preventDefault();
-    apiSubmitCredentials('sign-up', 'signup', () => alert("Successfully created account"));
-});
+const { useState } = React;
 
-document.getElementById('login').addEventListener("submit", (event) => {
-    event.preventDefault();
-    apiSubmitCredentials('login', 'login', () => window.location.href = "index.html");
-});
+const FormComponent = ({ event, Id, route, description }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangePassword = (e) => setPassword(e.target.value);
+
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <form
+          onSubmit={(e) => event(e, username, password, route)}
+          action={route}
+          id={`${Id}-form`}
+          method="POST"
+        >
+          <input
+            type="username"
+            required
+            name="username"
+            placeholder="Username"
+            onChange={onChangeUsername}
+          />
+          <input
+            minLength="6"
+            type="password"
+            required
+            name="password"
+            placeholder="Password"
+            onChange={onChangePassword}
+          />
+          <input type="submit" value={description} id={Id} />
+        </form>
+      </div>
+    </>
+  );
+};
+
+const LoginApp = () => {
+  const onSubmitOverride = (e, username, password, route) => {
+    e.preventDefault();
+    apiSubmitCredentials([username, password], route, () => {
+      if (route === "/login") window.location.href = "index.html";
+      else alert("Successfully created account");
+    });
+  };
+  return (
+    <>
+      <h1>Sign up</h1>
+      <br />
+      <FormComponent
+        event={onSubmitOverride}
+        Id="signup"
+        route="/signup"
+        description="Sign up"
+      />
+
+      <h1>Log in</h1>
+      <br />
+      <FormComponent
+        event={onSubmitOverride}
+        Id="login"
+        route="/login"
+        description="Log in"
+      />
+    </>
+  );
+};
+
+ReactDOM.render(<LoginApp />, document.getElementById("login-root"));
